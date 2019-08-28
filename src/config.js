345922ed7774b91ba9c63ec34f329180a468cc94
Matthew Willard <MWillard85@gmail.com>
2019-08-28 13:26:44 -0600

    updated server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  API_ENDPOINT: 'https://git.heroku.com/damp-savannah-81600.git',
+  TOKEN_KEY: 'all-for-one-sports-api-auth-token',
 }
