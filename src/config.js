d69f884df6b9712fae0609cef260669ef06ca6a7
Matthew Willard <MWillard85@gmail.com>
2019-08-31 12:47:37 -0600

    updated heroku url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://damp-savannah-81600.herokuapp.com',
+  API_ENDPOINT: 'https://damp-savannah-81600.herokuapp.com/api',
   TOKEN_KEY: 'all-for-one-sports-api-auth-token',
 }
