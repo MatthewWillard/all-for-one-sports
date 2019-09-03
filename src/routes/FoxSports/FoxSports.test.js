1d34a0271c2c25f01a7e81566696c4fc999aeefc
Matthew Willard <MWillard85@gmail.com>
2019-09-03 15:45:22 -0600

    added screenshots for readme

--- src/routes/FoxSports/FoxSports.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import FoxSports from './FoxSports';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<FoxSports />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
